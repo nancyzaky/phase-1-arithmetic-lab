6858d754880d77a3c156a1d3bbe85c270fddd193
Nancy Zaky <nancyzki@yahoo.com>
2021-07-11 22:24:25 -0700

    commit tree f0830d33d7a353298ddf13e5befeed80e43db631

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let multiply;
+let num1 = 62;
+let num2 = 1;
+let num3 = 24;
+let num4 = 5;
+multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+let mod = num3 % num4;
+let max = Math.max(7, 18, 20);
